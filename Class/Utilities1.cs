4e97d4abb61ee00c686920800373646480fc85d7
Rimsha98 <71652125+Rimsha98@users.noreply.github.com>
2020-09-21 23:11:20 +0500

    Add files via upload

---
+++ Class/Utilities1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace FypWeb.Class
+{
+    public static class Utilities1
+    {
+        
+        public static string GetConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["ApplicationServices"].ConnectionString.ToString();
+        }
+    }
+}
